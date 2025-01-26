f85d3a684d490fcabc2d0439bcd34e49ab5a6e35
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2025-01-25 20:32:35 -0500

    FRONTEND

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Hero from './Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+
+export default Home
